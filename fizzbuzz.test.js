631e5a19a14430ed5922b73487616d40f4364f08
Ian Whitmore <ianwhitmor@gmail.com>
2022-02-04 14:51:45 -0600

    two working tests that test that we can return a certain number

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,10 +1,10 @@
-const { expect, it } = require('@jest/globals');
-const { test } = require('picomatch');
-const { describe } = require('yargs');
+const { expect, it, test } = require('@jest/globals');
 const fizzbuzz = require('./fizzbuzz');
 
-test('fizzbuzz', () => {
-  it('OK, Fizzbuzz returns 1', () => {
-    expect(fizzbuzz(1)).to.equal(1);
-  })
+test('fizzbuzz returns 1', () => {
+    expect(fizzbuzz(1)).toBe(1);
+});
+
+test('fizzbuzz return 2', () => {
+  expect(fizzbuzz(2)).toBe(2);
 });
