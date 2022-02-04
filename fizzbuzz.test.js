e5c0be6adcc6ae5e3de057ae7fe36f47d5580fb9
Ian Whitmore <ianwhitmor@gmail.com>
2022-02-04 09:07:38 -0600

    fizzBuzz test

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,2 +1,7 @@
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
 const fizzbuzz = require('./fizzbuzz');
 
+test('fizzbuzz works', () => {
+  expect().toBe();
+});
