d699d393cbd2a06f5fb966ee4cca8b0925ac170a
Ian Whitmore <ianwhitmor@gmail.com>
2022-02-04 15:00:26 -0600

    Rewrite tests in AAA style

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -2,9 +2,19 @@ const { expect, it, test } = require('@jest/globals');
 const fizzbuzz = require('./fizzbuzz');
 
 test('fizzbuzz returns 1', () => {
-    expect(fizzbuzz(1)).toBe(1);
+  //Arrange 
+   let num = 1;
+   //Act
+   let result = fizzbuzz(num);
+  //Assert
+  expect(result).toBe(1);
 });
 
-test('fizzbuzz return 2', () => {
-  expect(fizzbuzz(2)).toBe(2);
+test('fizzbuzz returns 2', () => {
+  //Arrange
+  let num = 1;
+  //Act
+  let result = fizzbuzz(num);
+  //Assert
+  expect(result).toBe(1);
 });
