0cfcd8c71eef26b30f6a4d86b826dcb93e2ae87c
Ian Whitmore <ianwhitmor@gmail.com>
2022-02-04 15:24:12 -0600

    Refactor fizzbuzz()

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,13 @@
 function fizzbuzz(num) {
-  if (num % 15 === 0) return 'FizzBuzz';
-  if (num % 3 === 0) return 'Fizz';
-  if (num % 5 === 0) return 'Buzz';
+  for (let i = 1; i < 101; i++) {
+    if (num % 15 === 0) {
+      return 'FizzBuzz';
+    } else if (num % 3 === 0) {
+      return 'Fizz';
+    } else if (num % 5 === 0) {
+      return 'Buzz';
+    }
+  }
   return num;
 }
 
