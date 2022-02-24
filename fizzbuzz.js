0ceb0474360d03df16081d651b8e6ff09aaf2d83
Ian Whitmore <ianwhitmor@gmail.com>
2022-02-24 13:21:32 -0600

    Write FizzBuzz a second way

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,21 @@
+// function fizzbuzz(num) {
+//   for (let i = 0; i < 100; i++) {
+//     if (num % 15 === 0) return 'FizzBuzz'
+//     else if (num % 3 === 0) return 'Fizz';
+//     else if (num % 5 === 0) return 'Buzz';
+//   }
+//   return num;
+// }
+
 function fizzbuzz(num) {
   for (let i = 0; i < 100; i++) {
-    if (num % 15 === 0) return 'FizzBuzz'
-    else if (num % 3 === 0) return 'Fizz';
-    else if (num % 5 === 0) return 'Buzz';
+    if (num % 15 === 0) {
+      return 'FizzBuzz';
+    } else if (num % 3 === 0) {
+      return 'Fizz';
+    } else if (num % 5 === 0){
+      return 'Buzz';
+    }
   }
   return num;
 }
